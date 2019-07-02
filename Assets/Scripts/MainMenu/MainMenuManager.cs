499cd25e13f3b0d4268250f7790a34d51d941600
Mouadh Mkadmi <wvagen@gmail.com>
2019-07-02 14:34:54 +0100

    Version 2.0
    Phone compatibility bugs fixed

--- Assets/Scripts/MainMenu/MainMenuManager.cs
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine.SceneManagement;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -9,12 +10,41 @@ public class MainMenuManager : MonoBehaviour {
     public Transform pointOfViewPos;
     public Text infoTxt;
 
+    public Text loadingTxt;
+
     public SetInfoObject clickedPlanet;
     public static bool isPreviewingPlanet = false;
 
     public void LoadScene(string sceneName)
     {
-        SceneManager.LoadScene(sceneName);
+        if (loadingTxt != null)
+            StartCoroutine(LoadSceneCoroutine(sceneName));
+        else SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator LoadSceneCoroutine(string sceneName)
+    {
+        yield return null;
+
+        //Begin to load the Scene you specify
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(sceneName);
+        //Don't let the Scene activate until you allow it to
+        asyncOperation.allowSceneActivation = false;
+        Debug.Log("Pro :" + asyncOperation.progress);
+        //When the load is still in progress, output the Text and progress bar
+        while (!asyncOperation.isDone)
+        {
+            //Output the current progress
+            loadingTxt.text = "Loading progress:\n " + (asyncOperation.progress * 100) + "%";
+
+            // Check if the load has finished
+            if (asyncOperation.progress >= 0.9f)
+            {
+                    asyncOperation.allowSceneActivation = true;
+            }
+
+            yield return null;
+        }
     }
 
     public void ShowInfoPanel(string info)
