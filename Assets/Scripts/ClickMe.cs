0ad2b91861909f0b413c78215cb228c80ff19788
Mouadh Mkadmi <wvagen@gmail.com>
2019-10-08 20:40:23 +0100

    Version 3.0

---
+++ Assets/Scripts/ClickMe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickMe : MonoBehaviour
+{
+    public GameObject[] foods;
+    GameObject selectedFood;
+
+    void Update()
+    {
+        Debug.Log("Test");
+    }
+
+    void OnMouseDown()
+    {
+        if (EventSystem.current.gameObject.tag == "Table")
+        {
+            short randomIndex = (short) Random.Range(0, foods.Length);
+            if (selectedFood != null) selectedFood.SetActive(false);
+            foods[randomIndex].SetActive(true);
+            selectedFood = foods[randomIndex];
+        }
+    }
+}
